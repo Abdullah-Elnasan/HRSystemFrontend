28cfb30f0c81d66d873ef25f99040911e13c49aa
Abdullah-Elnasan <abo777hodefa@gmail.com>
2026-02-12 23:42:29 +0300

    payrollSystem2 store

--- app/composables/payrollSystem2/usePayrollSystems.ts
+++ app/composables/payrollSystem2/usePayrollSystems.ts
@@ -1,5 +1,5 @@
 // ~/composables/payroll-systems/usePayrollSystems.ts
-import { usePayrollSystemsStore } from '~/stores/payrollSystem/PayrollSystem'
+import { usePayrollSystemsStore } from '~/stores/payrollSystem2/PayrollSystem'
 import type { PayrollSystemForm } from '~/types/PayrollSystem'
 import { usePaginatedList } from '~/composables/usePaginatedList'
 
